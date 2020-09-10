da1d7556a174aee6b6d4c2b1594963f27ee02041
asungur <sunguralican@gmail.com>
2020-09-10 18:23:32 +0100

    signup errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
+  # SHOW USER PAGE
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  # SIGNUP FORM
   def new
+    @user = User.new
+  end
+
+  # SUBMIT FORM
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
+
